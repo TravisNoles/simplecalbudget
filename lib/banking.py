3b95327f1e6bad79e77b1e2364eab1f990caee6a
Travis Noles <travis@noles.pw>
2022-04-28 02:55:38 -0500

    Initial commit

---
+++ lib/banking.py
@@ -0,0 +1,28 @@
+class Bank(object):
+    def __init__(self):
+        self.name = ""
+        self.created = ""
+
+class Account(object):
+    def __init__(self):
+        self.name = ""
+        self.created = ""
+        self.balance = 0
+
+class Payee(object):
+    def __init__(self):
+        self.name = ""
+
+class Transaction(object):
+    def __init__(self):
+        self.name = ""
+        self.account = Account()
+        self.payee = Payee()
+
+
+
+
+
+
+
+
