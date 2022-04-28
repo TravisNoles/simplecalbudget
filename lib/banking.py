59a520fce7cd4f778313040f39136a2005c1e1e0
Travis Noles <travis@noles.pw>
2022-04-28 03:34:06 -0500

    adjusted banking classes and init functions

--- lib/banking.py
+++ lib/banking.py
@@ -17,8 +17,9 @@ class Transaction(object):
     def __init__(self):
         self.name = ""
         self.account = Account()
+        self.amount = 0
         self.payee = Payee()
-
+        self.description = ""
 
 
 
