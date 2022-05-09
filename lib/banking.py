b6a24bf3c5f6c7351f9a5277625e2652a18c533e
Travis Noles <travis@noles.pw>
2022-05-09 02:42:30 -0500

    planning banking logic.

--- lib/banking.py
+++ lib/banking.py
@@ -9,6 +9,9 @@ class Account(object):
         self.created = ""
         self.balance = 0
 
+    def addtransactiontobal(self, trans_id, amount):
+        self.balance = self.balance + amount
+
 class Payee(object):
     def __init__(self):
         self.name = ""
@@ -16,10 +19,18 @@ class Payee(object):
 class Transaction(object):
     def __init__(self):
         self.name = ""
+        self.id = 0
         self.account = Account()
         self.amount = 0
         self.payee = Payee()
         self.description = ""
+
+    def new(self):
+        #Pull incremental ID from database and add to local object instance.
+        #Check in with database, double check before adding to account balance, sanitize data.
+        print("stub")
+
+
 
 
 
